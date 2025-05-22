1f4fcdc792dbc633c4b3ea48e7c1874458d9e232
styanr <99873857+styanr@users.noreply.github.com>
2025-05-22 21:49:51 +0300

    BE: enhance GetStudyGroupsByUserHandler to filter study groups based on user role

--- backend/LangApp/LangApp.Infrastructure/EF/Queries/Handlers/StudyGroups/GetStudyGroupsByUserHandler.cs
+++ backend/LangApp/LangApp.Infrastructure/EF/Queries/Handlers/StudyGroups/GetStudyGroupsByUserHandler.cs
@@ -4,6 +4,8 @@ using LangApp.Application.StudyGroups.Dto;
 using LangApp.Application.StudyGroups.Queries;
 using LangApp.Infrastructure.EF.Context;
 using LangApp.Infrastructure.EF.Models.StudyGroups;
+using LangApp.Infrastructure.EF.Models.Users;
+using LangApp.Core.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace LangApp.Infrastructure.EF.Queries.Handlers.StudyGroups;
@@ -11,26 +13,43 @@ namespace LangApp.Infrastructure.EF.Queries.Handlers.StudyGroups;
 internal sealed class GetStudyGroupsByUserHandler : IQueryHandler<GetStudyGroupsByUser, PagedResult<StudyGroupSlimDto>>
 {
     private readonly DbSet<StudyGroupReadModel> _groups;
+    private readonly DbSet<UserReadModel> _users;
 
     public GetStudyGroupsByUserHandler(ReadDbContext context)
     {
         _groups = context.StudyGroups;
+        _users = context.Users;
     }
 
     public async Task<PagedResult<StudyGroupSlimDto>?> HandleAsync(GetStudyGroupsByUser query)
     {
-        var groups = await _groups
-            .Include(g => g.Members)
-            .Where(g => g.Members.Any(m => m.Id == query.UserId) || g.OwnerId == query.UserId)
-            .TakePage(query.PageNumber, query.PageSize)
+        var user = await _users
             .AsNoTracking()
+            .FirstOrDefaultAsync(u => u.Id == query.UserId);
+
+        if (user == null)
+        {
+            return new PagedResult<StudyGroupSlimDto>(
+                new List<StudyGroupSlimDto>(),
+                0,
+                query.PageNumber,
+                query.PageSize
+            );
+        }
+
+        var groupsQuery = _groups
+            .Include(g => g.Members)
+            .AsNoTracking();
+
+        groupsQuery = user.Role == UserRole.Teacher ? groupsQuery.Where(g => g.OwnerId == query.UserId) :
+            groupsQuery.Where(g => g.Members.Any(m => m.Id == query.UserId));
+
+        var groups = await groupsQuery
+            .TakePage(query.PageNumber, query.PageSize)
             .Select(g => new StudyGroupSlimDto(g.Id, g.Name, g.Language))
             .ToListAsync();
 
-        var count = await _groups
-            .Include(g => g.Members)
-            .Where(g => g.Members.Any(g => g.Id == query.UserId))
-            .CountAsync();
+        var count = await groupsQuery.CountAsync();
 
         return new PagedResult<StudyGroupSlimDto>(
             groups,
